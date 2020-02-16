cd0e990a4c82c0fa757fff46696b2202620163e5
Ben <btulkas@gmail.com>
2020-02-16 11:10:44 +0200

    Fixed sql export?

--- projectdb.sql
+++ projectdb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `project1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `project1`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: project1
@@ -73,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-16 10:59:23
+-- Dump completed on 2020-02-16 11:10:01
